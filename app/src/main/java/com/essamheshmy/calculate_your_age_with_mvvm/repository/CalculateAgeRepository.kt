7ef0f21280ffc8f50cbd3c659bbe45d0c8e63fd0
essam <essamheshmey>
2023-08-02 14:55:23 +0300

    Create CalculateAgeRepository clause .

---
+++ app/src/main/java/com/essamheshmy/calculate_your_age_with_mvvm/repository/CalculateAgeRepository.kt
@@ -0,0 +1,9 @@
+package com.essamheshmy.calculate_your_age_with_mvvm.repository
+
+import androidx.lifecycle.MutableLiveData
+import com.essamheshmy.calculate_your_age_with_mvvm.model.entity.Person
+
+
+interface CalculateAgeRepository {
+    fun calculateYourAge(name: String, age: Int): MutableLiveData<Person>
+}
