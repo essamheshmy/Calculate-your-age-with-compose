c4f53a35c264d5e6acbd7ac170d4d8bc20cf8e97
essam <essamheshmey>
2023-08-02 14:52:27 +0300

    عمل بعض التعديلات علي بعض الاصدرات واضافت بعض التبعية في ملف الجردل وعمل بعض التعديلا علي الشاشة الرئيسية

--- app/src/main/java/com/essamheshmy/calculate_your_age_with_mvvm/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/essamheshmy/calculate_your_age_with_mvvm/ui/screens/HomeScreen.kt
@@ -28,16 +28,28 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.essamheshmy.calculate_your_age_with_mvvm.model.entity.Person
 import com.essamheshmy.calculate_your_age_with_mvvm.ui.theme.btnModifier
 import com.essamheshmy.calculate_your_age_with_mvvm.ui.theme.textStyle
+import com.essamheshmy.calculate_your_age_with_mvvm.viewmodel.CalculateAgeViewModel
 
 @RequiresApi(Build.VERSION_CODES.Q)
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview
 @Composable
 fun homeScreen(){
+    var viewModel:CalculateAgeViewModel= viewModel()
+
     var name by rememberSaveable { mutableStateOf("") }
     var age by rememberSaveable { mutableStateOf(0) }
+    var namet by rememberSaveable { mutableStateOf("name") }
+
+    var person=Person("name",20)
+    var aget by rememberSaveable { mutableStateOf(0) }
 
     Column(modifier = Modifier
         .fillMaxSize()
@@ -46,20 +58,22 @@ fun homeScreen(){
         Column(
             Modifier.padding(50.dp,50.dp)
         ) {
+
             Text(
-                text = "Your Name",
+                text = "Your $namet",
                 fontSize = 30.sp,
                 fontStyle = androidx.compose.ui.text.font.FontStyle(FontStyle.FONT_WEIGHT_BOLD)
             )
             Text(
-                text = "Your Age",
+                text = "Your $aget",
                 fontSize = 30.sp,
                 fontStyle = androidx.compose.ui.text.font.FontStyle(FontStyle.FONT_WEIGHT_BOLD)
             )
         }
 
         Column(
-            modifier=Modifier.fillMaxSize()
+            modifier= Modifier
+                .fillMaxSize()
                 .padding(horizontal = 16.dp),
             verticalArrangement = Arrangement.Center,
         ) {
@@ -81,11 +95,17 @@ fun homeScreen(){
 
             Button(
                 onClick = {
-                          name="essam"
+              //viewModel.calculiteAge2("name,age",20)
+                   // name=person.name.toString()
+                   //person.name.toString()
+                  //  person.age!!
+
+                    aget=age
+                    namet=name
                 },
                 modifier= Modifier
-                       .fillMaxWidth()
-                       .height(54.dp),
+                    .fillMaxWidth()
+                    .height(54.dp),
                 shape = RoundedCornerShape(10.dp)
             )
             { Text(text = "Button with gray background",color = Color.White) }
