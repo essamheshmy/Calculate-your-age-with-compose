87ac0f9acabac808201fa7287916a1c8c6cde063
essam <essamheshmey>
2023-09-15 12:48:55 +0300

    null comint  :)2

--- app/src/main/java/com/essamheshmy/calculate_your_age_with_mvvm/presentation/scrins/list_person_scrin/PersonListViewModel.kt
+++ app/src/main/java/com/essamheshmy/calculate_your_age_with_mvvm/presentation/scrins/list_person_scrin/PersonListViewModel.kt
@@ -3,7 +3,7 @@ package com.essamheshmy.calculate_your_age_with_mvvm.presentation.scrins.list_pe
 import androidx.lifecycle.ViewModel
 import com.heshmy.data.local.PersonDao
 
-class PersonListViewModel: ViewModel() {
+/*class PersonListViewModel: ViewModel() {
     val personList: com.heshmy.data.repository.LocalRepository =
-        com.heshmy.data.repository.LocalRepositoryImpl(PersonDao)
-}
+        com.heshmy.data.repository.LocalRepositoryImpl(PersonDao())
+}*/
